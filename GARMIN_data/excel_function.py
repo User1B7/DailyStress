1e76daf7e1624b68d6b51b570618d591712e55e0
User1B7 <115086838+User1B7@users.noreply.github.com>
2024-03-26 00:33:58 +0100

    Corrected to load data from Google Drive

--- GARMIN_data/excel_function.py
+++ GARMIN_data/excel_function.py
@@ -1,8 +1,27 @@
 import pandas as pd
+import json
+import warnings
+
+warnings.simplefilter("ignore")
+
 
 def get_excel_data():
-    df = pd.read_csv("./GARMIN_data/excel/stundenbuch.csv")
-    df.columns = ["Zeitstempel","Mail","Start","Gefuehl Anfang", "Konzentration", "Gefuehl Ende", "Pausen", "Ende"]
+    url2 = "https://docs.google.com/spreadsheets/d/1-5QijnBttDqLJFZssCqQ4vqi7CBir6LY4ctp8qOqwQw/edit#gid=1351272459"
+    url_part1 = "/".join(url2.split("/")[:-1])
+    url_part2 = url2.split("/")[-1][-10:]
+    new_url = f"{url_part1}/export?gid={url_part2}&format=csv"
+    df = pd.read_csv(new_url, sep=",", on_bad_lines="skip")
+    df.columns = [
+        "Zeitstempel",
+        "Mail",
+        "Start",
+        "Gefuehl Anfang",
+        "Konzentration",
+        "Gefuehl Ende",
+        "Pausen",
+        "Ende",
+    ]
+    print(df.head())
     df["Zeitstempel"] = [d[0:10] for d in df["Zeitstempel"]]
     df.fillna(0, inplace=True)
     df["Pausen"] = [str(d).replace(" ", "").replace("bis", ",") for d in df["Pausen"]]
@@ -18,7 +37,21 @@ def get_excel_data():
             df["Pause Ende"][i] = 0
     return df
 
-if __name__ == "__main__":
+
+def sort_by_names():
     df = get_excel_data()
+    with open("./data/names.json") as f:
+        data = json.load(f)
+    print(data)
+    print(type(data))
+    df["Name"] = ""
+    for i in range(len(df["Mail"])):
+        if df["Mail"][i] in data:
+            df["Name"][i] = data[df["Mail"][i]]
+    return df
+
+
+if __name__ == "__main__":
+    df = sort_by_names()
     print(df)
     # print(df.shape)
